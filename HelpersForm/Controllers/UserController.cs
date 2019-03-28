4d9a448134f52f9b14700bf7a708cb706ca08e1a
Dawich Rafael <dawichelo@gmail.com>
2019-03-28 04:59:27 -0400

    holi

---
+++ HelpersForm/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using HelpersForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpersForm.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("VerUser", user);
+            }
+            else return View();
+    
+        
+        }
+        public ActionResult VerUser(Usuario x)
+        {
+            return View(x);
+        }
+
+    }
+}
---
+++ HelpersForm/Models/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelpersForm.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage ="Llene este campo")]
+        [MaxLength(15, ErrorMessage ="No se permiten mas de 15 caracteres")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "Llene este campo")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Llene este campo")]
+        public string Apellido { get; set; }
+
+        [Required]
+        [Range(15, 100, ErrorMessage = "Su edad debe ser mayor que 15")]
+        public short Edad { get; set; }
+
+        [Required(ErrorMessage = "Llene este campo")]
+        public string Telefono { get; set; }
+
+       
+        [EmailAddress(ErrorMessage ="Ingrese un correo valido")]
+        public string Correo { get; set; }
+
+        [Required(ErrorMessage = "Llene este campo")]
+        public Generos Genero { get; set; }
+
+        [Required(ErrorMessage = "Llene este campo")]
+        public string Estado_Civil { get; set; }
+
+        public bool Pasatiempos_1 { get; set; }
+        public bool Pasatiempos_2 { get; set; }
+        public bool Pasatiempos_3 { get; set; }
+        public bool Pasatiempos_4 { get; set; }
+
+    }
+
+    public enum Generos
+    {
+        Masculino,
+        Femenino
+    }
+
+}
